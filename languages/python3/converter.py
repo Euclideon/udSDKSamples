6840b3dff8784d9f22edc9c7821a602c1bffe585
Braden Wockner <bwockner@euclideon.com>
2020-03-26 15:04:07 +1000

    Cleaned up main and error handling of python converter.py Support merge or separate UDS conversion of multiple files Python API now searches for  dll in VAULTSDK_HOME if it cannot find it in the current working directory

--- languages/python3/converter.py
+++ languages/python3/converter.py
@@ -4,94 +4,143 @@
 #  A Vault Client Hello World! program in Python3.7   #
 #######################################################
 
-#######################################################
-###################### Imports ########################
-#######################################################
-import vault
+"""
+converter.py
 
-from ctypes import c_int, c_float
+General purpose command line converter for Euclideon Vault using python
+Requires Vault SDK shared libraries (.so or .dll)
+Converts any point cloud format supported by Vault SDK to UDS format 
+
+By default will produce one UDS for each model provided
+If no models are provided the program will attempt to convert the sample file located
+at ../../samplefiles/DirCube.uds
+
+--merge produces a single uds from the input files 
+
+output files are stored in:
+    ./convertedUDS/[inputName].uds for individual mode
+    ./mergedUDS/[firstInputName].uds for merge mode
+    
+    
+Usage:
+    converter username password [models] [--merge]
+"""
+
+import vault
 import os
-import platform
-from os import getcwd
-from os.path import dirname, basename, abspath
+from os.path import abspath
 from sys import exit
-from PIL import Image
 from sys import argv
 
 #######################################################
 ####################### Setup #########################
 #######################################################
-cwd = getcwd()
 
-SDKPath = os.environ.get("VAULTSDK_HOME")
-if SDKPath and platform.system() == 'Windows':
-    SDKPath +="/lib/win_x64/vaultSDK"
-    #TODO add checks for other operating systems
-else:#we assume that the .dll or .so file is in the current working directory
-    SDKPath = abspath("./vaultSDK")
-
-modelFiles = [abspath("../../samplefiles/DirCube.uds")]
-outFile = abspath("./ConvertedUDS.uds")
+# Load the SDK and fetch symbols
+SDKPath = abspath("./vaultSDK") #this is where we will look for the dll first
+vault.LoadVaultSDK(SDKPath)
+vaultSDK = vault.vaultSDK
 
 appName = "PythonSample_Convert"
 
+#some default values; these should be overwritten by argv
+modelFiles = [abspath("../../samplefiles/DirCube.uds")]
+outFile = abspath("./ConvertedUDS.uds")
 serverPath = "https://earth.vault.euclideon.com"
 userName = "Username"
 userPass = "Password"
 
-if len(argv) >= 3:
-  userName = argv[1]
-  userPass = argv[2]
+vaultContext = vault.vdkContext()
+convertContext = vault.vdkConvertContext()
 
-if len(argv) >= 4:
-  modelFiles = argv[3:]
-  
-def convertModel(modelFile, outFile):
-    # Load the SDK and fetch symbols
-    vault.LoadVaultSDK(SDKPath)
+def vault_login():
+    """
+    to vault server and requests a license
 
-    vaultSDK = vault.vaultSDK
+    Returns
+    -------
+    None.
 
-    # Do the thing
-    vaultContext = vault.vdkContext()
-    convertContext = vault.vdkConvertContext()
-
-
+    """
     try:
-      vaultContext.Connect(serverPath, appName, userName, userPass)
-      vaultContext.RequestLicense(vault.vdkLicenseType.Convert)
-      vdkError = convertContext.Create(vaultContext)
-      vdkError = convertContext.AddItem(modelFile)
-      vdkError = convertContext.Output(outFile)
-      
-      print("Converting...")
-      convertContext.DoConvert()
-      input("Conversion successful! Press Enter to continue...")
-      
-      # Exit gracefully
+        vaultContext.Connect(serverPath, appName, userName, userPass)
+        vaultContext.RequestLicense(vault.vdkLicenseType.Convert)
+        convertContext.Create(vaultContext)
+    except vault.VdkException as err:
+        err.printout()
+        exit()
+
+def vault_logout():
+        # Exit gracefully
       convertContext.Destroy()
       vaultContext.Disconnect()
-    except Exception as err:
-      if len(err.args) == 2:
-        vaultError = err.args[1]
-        if (vaultError == vault.vdkError.ConnectionFailure):
-            print("Could not connect to server.")
-        elif (vaultError == vault.vdkError.NotAllowed):
-            print("Username or Password incorrect.")
-        elif (vaultError == vault.vdkError.OutOfSync):
-            print("Your clock doesn't match the remote server clock.")
-        elif (vaultError == vault.vdkError.SecurityFailure):
-            print("Could not open a secure channel to the server.")
-        elif (vaultError == vault.vdkError.ServerFailure):
-            print("Unable to negotiate with server, please confirm the server address")
-        elif (vaultError != vault.vdkError.Success):
-            print("Error: ", vault.vdkError(vaultError).name)
+  
+def convert_model(modelFiles, outFile):
+    """
+    performs a conversion of a list of input files to the output UDS at path outfile
+
+    Parameters
+    ----------
+    modelFiles : List[string]
+        List of paths to files to be converted
+    outFile : string
+        path to the output file to be written
+
+    Returns
+    -------
+    None.
+
+    """
+
+    try:
+      
+      formattedInputNames = ""
+      for modelFile in modelFiles:
+          vdkError = convertContext.AddItem(modelFile)
+          formattedInputNames += "\t {}\n".format(modelFile)
+      vdkError = convertContext.Output(outFile)
+      
+      print("Converting files:\n {} to {}".format(formattedInputNames,outFile))
+      convertContext.DoConvert()
+      #input("Conversion successful! Press Enter to continue...")
+      print("done")
+      
+      
+    except vault.VdkException as err:
+      err.printout();
     
 
 #######################################################
 ######################## Main #########################
 #######################################################
 if __name__ == "__main__":
-    for modelFile in modelFiles:
-        outFile = abspath("./converted_"+modelFile)
-        convertModel(modelFile, outFile)
+    if len(argv)<3:
+        print("Usage: {} VaultUserName VaultPassword [--merge] [inputFiles]".format(argv[0]))
+    try:
+        argv.remove("--merge")
+        merge = True
+    except ValueError:
+        merge = False
+    
+    #mass convert, single directory, individual output files
+    if len(argv) >= 3:
+        userName = argv[1]
+        userPass = argv[2]
+
+    if len(argv) >= 4:
+        modelFiles = argv[3:]
+    else:
+        print("No model specified, falling back to example uds at {}".format(modelFiles[0]))
+        
+    vault_login()
+    
+    if merge:
+        outFile = abspath("./mergedUDS/"+os.path.basename(modelFiles[0])+".uds")
+        convert_model(modelFiles,outFile)
+    else:
+        for modelFile in modelFiles:
+            outFile = os.path.splitext(modelFile)[0]
+            outFile = abspath("./convertedUDS/"+os.path.basename(outFile)+".uds")
+            convert_model([modelFile], outFile)
+    
+    vault_logout()
